52b5e990e112c484ab73ee9c7761a50e6f755284
ryan75195 <ryan75195@gmail.com>
2026-07-15 18:33:43 +0100

    Add durable template orchestrator unit tests

--- dotnet/templates/durable/tests/SampleDurable.Tests.Unit/Functions/Orchestrations/AgentRunOrchestratorTests.cs
+++ dotnet/templates/durable/tests/SampleDurable.Tests.Unit/Functions/Orchestrations/AgentRunOrchestratorTests.cs
@@ -1,4 +1,9 @@
 using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.DurableTask;
+using NSubstitute;
+using SampleDurable.Core.Models;
+using SampleDurable.Functions.Activities;
 using SampleDurable.Functions.Orchestrations;
 
 namespace SampleDurable.Tests.Unit.Functions.Orchestrations;
@@ -21,5 +26,54 @@ public class AgentRunOrchestratorTests
         var second = AgentRunOrchestrator.SubInstanceId("run-key-1", "item-2");
 
         first.Should().NotBe(second);
+    }
+
+    [Test]
+    public async Task Should_aggregate_sub_orchestration_results_into_a_summary()
+    {
+        var context = Substitute.For<TaskOrchestrationContext>();
+
+        context
+            .CallSubOrchestratorAsync<AgentResult>(
+                Arg.Is<TaskName>(n => n.Name == nameof(AgentTaskOrchestrator)),
+                Arg.Any<object>(),
+                Arg.Any<TaskOptions>())
+            .Returns(
+                Task.FromResult(new AgentResult("a", true, "ok")),
+                Task.FromResult(new AgentResult("b", false, "timed out")));
+
+        var request = new AgentRunRequest("key-1",
+        [
+            new AgentWorkItem("a", "p"),
+            new AgentWorkItem("b", "p")
+        ]);
+
+        var summary = await AgentRunOrchestrator.Run(context, request);
+
+        using (new AssertionScope())
+        {
+            summary.Total.Should().Be(2);
+            summary.Succeeded.Should().Be(1);
+            summary.TimedOut.Should().Be(1);
+        }
+    }
+
+    [Test]
+    public async Task Should_publish_the_summary()
+    {
+        var context = Substitute.For<TaskOrchestrationContext>();
+        context
+            .CallSubOrchestratorAsync<AgentResult>(
+                Arg.Any<TaskName>(), Arg.Any<object>(), Arg.Any<TaskOptions>())
+            .Returns(Task.FromResult(new AgentResult("a", true, "ok")));
+
+        var request = new AgentRunRequest("key-1", [new AgentWorkItem("a", "p")]);
+
+        await AgentRunOrchestrator.Run(context, request);
+
+        await context.Received(1).CallActivityAsync(
+            Arg.Is<TaskName>(n => n.Name == nameof(PublishSummaryActivity)),
+            Arg.Any<object>(),
+            Arg.Any<TaskOptions>());
     }
 }
--- dotnet/templates/durable/tests/SampleDurable.Tests.Unit/Functions/Orchestrations/AgentTaskOrchestratorTests.cs
+++ dotnet/templates/durable/tests/SampleDurable.Tests.Unit/Functions/Orchestrations/AgentTaskOrchestratorTests.cs
@@ -1,4 +1,10 @@
 using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Entities;
+using NSubstitute;
+using SampleDurable.Core.Models;
+using SampleDurable.Functions.Activities;
 using SampleDurable.Functions.Orchestrations;
 
 namespace SampleDurable.Tests.Unit.Functions.Orchestrations;
@@ -6,15 +12,86 @@ namespace SampleDurable.Tests.Unit.Functions.Orchestrations;
 [TestFixture]
 public class AgentTaskOrchestratorTests
 {
-    [Test]
-    public void Should_expose_the_agent_completed_event_name_used_by_the_callback_trigger()
+    private static TaskOrchestrationContext CreateContext()
     {
-        AgentTaskOrchestrator.AgentCompletedEventName.Should().Be("AgentCompleted");
+        var context = Substitute.For<TaskOrchestrationContext>();
+        context.CurrentUtcDateTime.Returns(new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+        context.InstanceId.Returns("run-abc");
+        context.Entities.Returns(Substitute.For<TaskOrchestrationEntityFeature>());
+        return context;
+    }
+
+    private static void StubDispatch(TaskOrchestrationContext context)
+    {
+        context
+            .CallActivityAsync<AgentDispatch>(
+                Arg.Is<TaskName>(n => n.Name == nameof(DispatchAgentActivity)),
+                Arg.Any<object>(),
+                Arg.Any<TaskOptions>())
+            .Returns(Task.FromResult(new AgentDispatch("d-1", "item-1")));
     }
 
     [Test]
-    public void Should_expose_a_two_hour_dispatch_timeout()
+    public async Task Should_return_timed_out_result_when_the_timer_wins()
     {
-        AgentTaskOrchestrator.DispatchTimeout.Should().Be(TimeSpan.FromHours(2));
+        var context = CreateContext();
+        StubDispatch(context);
+
+        context
+            .WaitForExternalEvent<AgentResult>(AgentTaskOrchestrator.AgentCompletedEventName)
+            .Returns(new TaskCompletionSource<AgentResult>().Task);
+        context
+            .CreateTimer(Arg.Any<DateTime>(), Arg.Any<CancellationToken>())
+            .Returns(Task.CompletedTask);
+
+        var result = await AgentTaskOrchestrator.Run(context, new AgentWorkItem("item-1", "p"));
+
+        using (new AssertionScope())
+        {
+            result.Succeeded.Should().BeFalse();
+            result.WorkItemId.Should().Be("item-1");
+            result.Output.Should().Be("timed out");
+        }
+    }
+
+    [Test]
+    public async Task Should_return_the_agent_result_when_the_callback_wins()
+    {
+        var context = CreateContext();
+        StubDispatch(context);
+
+        context
+            .WaitForExternalEvent<AgentResult>(AgentTaskOrchestrator.AgentCompletedEventName)
+            .Returns(Task.FromResult(new AgentResult("item-1", true, "done")));
+        context
+            .CreateTimer(Arg.Any<DateTime>(), Arg.Any<CancellationToken>())
+            .Returns(new TaskCompletionSource().Task);
+
+        var result = await AgentTaskOrchestrator.Run(context, new AgentWorkItem("item-1", "p"));
+
+        using (new AssertionScope())
+        {
+            result.Succeeded.Should().BeTrue();
+            result.Output.Should().Be("done");
+        }
+    }
+
+    [Test]
+    public async Task Should_dispatch_before_waiting_for_the_callback()
+    {
+        var context = CreateContext();
+        StubDispatch(context);
+        context
+            .WaitForExternalEvent<AgentResult>(AgentTaskOrchestrator.AgentCompletedEventName)
+            .Returns(Task.FromResult(new AgentResult("item-1", true, "done")));
+        context.CreateTimer(Arg.Any<DateTime>(), Arg.Any<CancellationToken>())
+            .Returns(new TaskCompletionSource().Task);
+
+        await AgentTaskOrchestrator.Run(context, new AgentWorkItem("item-1", "p"));
+
+        await context.Received(1).CallActivityAsync<AgentDispatch>(
+            Arg.Is<TaskName>(n => n.Name == nameof(DispatchAgentActivity)),
+            Arg.Any<object>(),
+            Arg.Any<TaskOptions>());
     }
 }
